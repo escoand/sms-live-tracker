57e8ed44ca6a48af8bd9d3fa156199bd2543c245
escoand <escoand@users.noreply.github.com>
2024-10-15 09:37:23 +0200

    fcgiwrap use sock file

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,7 +13,7 @@ chmod 0644 "$POSITIONS"
 
 # start frontend
 sed -i "s|{POSITIONS_DIR}|$POSDIR|; s|{POSITIONS_FILE}|$POSFILE|" /etc/nginx/http.d/default.conf
-spawn-fcgi -p 9000 -f /usr/bin/fcgiwrap
+spawn-fcgi -f /usr/bin/fcgiwrap -s /run/fcgiwrap.sock -u nobody -g nobody
 nginx
 
 # switch to modem mode
