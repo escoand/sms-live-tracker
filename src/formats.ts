7da6ba18d2f9d563cc0a228ecda932b98f4355e9
escoand <escoand@users.noreply.github.com>
2026-01-08 10:19:48 +0100

    better format conversion

--- src/formats.ts
+++ src/formats.ts
@@ -8,42 +8,64 @@ import {
   Position,
 } from "geojson";
 
-const gpxNsPattern = /^http:\/\/www\.topografix\.com\/GPX\/\d+\/\d+$/;
-const gpxNs = "http://www.topografix.com/GPX/1/1";
-
-type gpxTypes =
-  | "degreesType"
-  | "dgpsStationType:integer"
-  | "fixType"
-  | "hexColor"
-  | "linkType"
-  | "xsd:dateTime"
-  | "xsd:decimal"
-  | "xsd:nonNegativeInteger"
-  | "xsd:string";
-
-const gpxAttributes: { [k: string]: { property?: string; type: gpxTypes } } = {
-  ageofdgpsdata: { type: "xsd:decimal" },
-  desc: { type: "xsd:string", property: "description" },
-  dgpsid: { type: "dgpsStationType:integer" },
-  fix: { type: "fixType" },
-  geoidheight: { type: "xsd:decimal" },
-  hdop: { type: "xsd:decimal" },
-  link: { type: "linkType" },
-  magvar: { type: "degreesType" },
-  pdop: { type: "xsd:decimal" },
-  sat: { type: "xsd:nonNegativeInteger" },
-  sym: { type: "xsd:string", property: "marker-symbol" },
-  time: { type: "xsd:dateTime" },
-  vdop: { type: "xsd:decimal" },
+type Mapping = {
+  property: string;
+  type:
+    | "degreesType"
+    | "dgpsStationType:integer"
+    | "fixType"
+    | "hexColor"
+    | "linkType"
+    | "xsd:dateTime"
+    | "xsd:decimal"
+    | "xsd:nonNegativeInteger"
+    | "xsd:string";
+  xpath: string;
 };
 
-const geojsonProperties = Object.fromEntries(
-  Object.entries(gpxAttributes).map(([tag, values]) => [
-    values.property || tag,
-    { type: values.type, tag: tag },
-  ])
-);
+const namespaces: { [_: string]: string } = {
+  gpx: "http://www.topografix.com/GPX/1/1",
+  gpxx: "http://www.garmin.com/xmlschemas/GpxExtensions/v3",
+  osmand: "http://osmand.net",
+};
+
+const namespaceResolver: XPathNSResolver = (prefix) =>
+  (prefix && namespaces[prefix]) || null;
+
+const mappings: Mapping[] = [
+  {
+    property: "ageofdgpsdata",
+    type: "xsd:decimal",
+    xpath: "gpx:ageofdgpsdata",
+  },
+  { property: "description", type: "xsd:string", xpath: "gpx:desc" },
+  { property: "dgpsid", type: "dgpsStationType:integer", xpath: "gpx:dgpsid" },
+  { property: "fix", type: "fixType", xpath: "gpx:fix" },
+  { property: "geoidheight", type: "xsd:decimal", xpath: "gpx:geoidheight" },
+  { property: "hdop", type: "xsd:decimal", xpath: "gpx:hdop" },
+  { property: "link", type: "linkType", xpath: "gpx:link" },
+  { property: "magvar", type: "degreesType", xpath: "gpx:magvar" },
+  { property: "marker-symbol", type: "xsd:string", xpath: "gpx:sym" },
+  { property: "name", type: "xsd:string", xpath: "gpx:name" },
+  { property: "pdop", type: "xsd:decimal", xpath: "gpx:pdop" },
+  { property: "sat", type: "xsd:nonNegativeInteger", xpath: "gpx:sat" },
+  { property: "time", type: "xsd:dateTime", xpath: "gpx:time" },
+  { property: "vdop", type: "xsd:decimal", xpath: "gpx:vdop" },
+  // Garmin
+  {
+    property: "color",
+    type: "xsd:string",
+    xpath: "gpx:extensions/gpxx:TrackExtension/gpxx:DisplayColor",
+  },
+  // OsmAnd
+  {
+    property: "color",
+    type: "xsd:string",
+    xpath: "gpx:extensions/osmand:color",
+  },
+  // not standardized
+  { property: "color", type: "xsd:string", xpath: "gpx:extensions/gpx:color" },
+];
 
 export const toFeatures = (data: GeoJSON): Feature[] =>
   data.type === "Feature"
@@ -139,7 +161,11 @@ export const toGPX = (
   json: GeoJSON,
   trackType: "trk" | "rte" = "trk"
 ): Document => {
-  const doc = document.implementation.createDocument(gpxNs, "gpx", null);
+  const doc = document.implementation.createDocument(
+    namespaces.gpx,
+    "gpx",
+    null
+  );
   doc.insertBefore(
     doc.createProcessingInstruction("xml", 'version="1.0" encoding="UTF-8"'),
     doc.documentElement
@@ -158,7 +184,7 @@ export const toGPX = (
     // point
     if (feature.geometry.type === "Point") {
       const wpt = doc.documentElement.appendChild(
-        document.createElementNS(gpxNs, "wpt")
+        document.createElementNS(namespaces.gpx, "wpt")
       );
       _addCoords(wpt, feature.geometry.coordinates);
       _mapToTags(feature.properties, wpt);
@@ -170,16 +196,18 @@ export const toGPX = (
       feature.geometry.type === "MultiLineString"
     ) {
       const track = doc.documentElement.appendChild(
-        document.createElementNS(gpxNs, "trk")
+        document.createElementNS(namespaces.gpx, "trk")
       );
       _mapToTags(feature.properties, track);
       feature.geometry.coordinates.forEach((coords) => {
         const segment = track.appendChild(
-          document.createElementNS(gpxNs, "trkseg")
+          document.createElementNS(namespaces.gpx, "trkseg")
         );
         coords.forEach((coord) =>
           _addCoords(
-            segment.appendChild(document.createElementNS(gpxNs, "trkpt")),
+            segment.appendChild(
+              document.createElementNS(namespaces.gpx, "trkpt")
+            ),
             coord
           )
         );
@@ -189,14 +217,16 @@ export const toGPX = (
     // line as route
     else if (feature.geometry.type === "MultiLineString") {
       const route = doc.documentElement.appendChild(
-        document.createElementNS(gpxNs, "rte")
+        document.createElementNS(namespaces.gpx, "rte")
       );
       _mapToTags(feature.properties, route);
       feature.geometry.coordinates
         .flat()
         .forEach((coord) =>
           _addCoords(
-            route.appendChild(document.createElementNS(gpxNs, "rtept")),
+            route.appendChild(
+              document.createElementNS(namespaces.gpx, "rtept")
+            ),
             coord
           )
         );
@@ -208,46 +238,57 @@ export const toGPX = (
 
 const _mapToProperties = (node: Element) =>
   Object.fromEntries(
-    Array.from(node.children)
-      .filter(
-        (child) =>
-          ((child.namespaceURI && gpxNsPattern.test(child.namespaceURI)) ||
-            child.namespaceURI === null) &&
-          !["ele", "rtept", "trkpt", "trkseg"].includes(child.nodeName)
-      )
-      .map((child) => [
-        gpxAttributes[child.nodeName]?.property || child.nodeName,
-        gpxAttributes[child.nodeName]?.type === "xsd:dateTime"
-          ? new Date(child.textContent)
-          : gpxAttributes[child.nodeName]?.type === "xsd:decimal"
-          ? parseFloat(child.textContent)
-          : gpxAttributes[child.nodeName]?.type === "xsd:nonNegativeInteger"
-          ? parseInt(child.textContent)
-          : child.textContent,
-      ])
+    mappings
+      .map((mapping) => {
+        const result = node.ownerDocument.evaluate(
+          mapping.xpath,
+          node,
+          namespaceResolver,
+          XPathResult.FIRST_ORDERED_NODE_TYPE
+        );
+        if (result.singleNodeValue === null) return;
+        const value = !result.singleNodeValue.textContent
+          ? null
+          : mapping.type === "xsd:dateTime"
+          ? new Date(result.singleNodeValue.textContent)
+          : mapping.type === "xsd:decimal"
+          ? parseFloat(result.singleNodeValue.textContent)
+          : mapping.type === "xsd:nonNegativeInteger"
+          ? parseInt(result.singleNodeValue.textContent)
+          : result.singleNodeValue.textContent;
+        return [mapping.property, value];
+      })
+      .filter((_) => _ !== undefined)
   );
 
-const _mapToTags = (properties: GeoJsonProperties, node: Element) =>
-  Object.entries(properties || {}).forEach(([property, values]) =>
-    node
-      .appendChild(
-        document.createElementNS(
-          gpxNs,
-          geojsonProperties[property]?.tag || property
-        )
-      )
-      .append(
-        geojsonProperties[property]?.type === "xsd:dateTime"
-          ? new Date(values).toISOString().replace(/\.\d+Z$/, "Z")
-          : values
-      )
-  );
+const _mapToTags = (properties: GeoJsonProperties, node: Element) => {
+  properties &&
+    mappings.forEach((mapping) => {
+      if (!(mapping.property in properties)) return;
+      let _node = node;
+      mapping.xpath.split("/").forEach((part) => {
+        const [prefix, name] = part.split(":");
+        const namespace = namespaceResolver(prefix);
+        _node =
+          Array.from(_node.children).find(
+            (_) => _.namespaceURI === namespace && _.nodeName === name
+          ) || _node.appendChild(document.createElementNS(namespace, name));
+      });
+      _node.append(
+        mapping.type === "xsd:dateTime"
+          ? new Date(properties[mapping.property])
+              .toISOString()
+              .replace(/\.\d+Z$/, "Z")
+          : properties[mapping.property]
+      );
+    });
+};
 
 const _addCoords = (segment: Element, coords: Position) => {
   segment.setAttribute("lat", coords[1].toString());
   segment.setAttribute("lon", coords[0].toString());
   coords[2] &&
     segment
-      .appendChild(document.createElementNS(gpxNs, "ele"))
+      .appendChild(document.createElementNS(namespaces.gpx, "ele"))
       .append(coords[2].toString());
 };
